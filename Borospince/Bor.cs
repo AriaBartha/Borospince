b2c4bb978b6259be2d882218f3213fc17186c61e
Bartha Ariana <ariabartha@gmail.com>
2025-06-13 10:24:43 +0200

    Bor osztály

--- Borospince/Bor.cs
+++ Borospince/Bor.cs
@@ -8,5 +8,33 @@ namespace Borospince
 {
     internal class Bor
     {
+        private string fajta;
+        private int evjarat;
+        private double alkoholtartalom;
+
+        public Bor(string fajta, int evjarat, double alkoholtartalom = 12.5)
+        {
+            this.Fajta = fajta;
+            this.Evjarat = evjarat;
+            this.Alkoholtartalom = alkoholtartalom;
+        }
+
+        public string Fajta { get => fajta; set => fajta = value; }
+        public int Evjarat { get => evjarat; set => evjarat = value; }
+        public double Alkoholtartalom 
+        { get => alkoholtartalom; 
+          set 
+            { 
+                if(value < 0 || value > 100)
+                {
+                    throw new BorospinceException("Nem megfelelő alkoholtartalom!");
+                    
+                }
+                else
+                {
+                    alkoholtartalom = value;
+                }
+            } 
+        }
     }
 }
