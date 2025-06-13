8d27cda631dc2ca0ee2c21591d5d366def908934
Bartha Ariana <ariabartha@gmail.com>
2025-06-13 09:26:47 +0200

    Add project files.

---
+++ Borospince/BorospinceException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borospince
+{
+    internal class BorospinceException : Exception
+    {
+        public BorospinceException(string message) : base(message) { }
+        
+    }
+}
